264808c64e26eb2d3e9c838a54fcb124d6ec2e41
dimwarlock <Screanvir@gmail.com>
2022-10-25 02:13:39 -0300

    Version mas Indiscutible que la anterior.

--- Propuesta IA Alternativa/Main.java
+++ Propuesta IA Alternativa/Main.java
@@ -1,15 +1,34 @@
+import java.util.*;
+
 public class Main
 {    
     public static void main (String [] args)
     {
-        //TEST I & II [Experimental] - CONTRA UN USUARIO HUMANO
-        //JuegoDelGatoIAvsUsuario Gato = new JuegoDelGatoIAvsUsuario ();
-        //Gato.JugarGato();
+        Scanner Teclado = new Scanner (System.in);
 
-        //TEST III & IV [Experimental] - IA VS IA
-        //JuegoDelGatoIAvsIA Gato2 = new JuegoDelGatoIAvsIA ();
-        //Gato2.JugarGato();
+        System.out.println ("Escriba el N° del Modo de Juego que desea ejecutar.");
+        System.out.println ("1 = Agente vs Usuario | 2 = Agente vs Agente");
 
+        int ModoDeJuegoPorSupuesto = Teclado.nextInt();
+
+        if (ModoDeJuegoPorSupuesto == 1)
+        {
+            //TEST I & II [Experimental] - CONTRA UN USUARIO HUMANO
+            JuegoDelGatoIAvsUsuario Gato = new JuegoDelGatoIAvsUsuario ();
+            Gato.JugarGato();
+        }
+        else if (ModoDeJuegoPorSupuesto == 2)
+        {
+            //TEST III & IV [Experimental] - IA VS IA
+            JuegoDelGatoIAvsIA Gato2 = new JuegoDelGatoIAvsIA ();
+            Gato2.JugarGato();
+        }
+        else
+        {
+            System.out.print ("No se ha ingresado un Modo de Juego. *Procede a cerrarse*");
+        }
+
+        Teclado.close();
 
         /*Por que cuando es Agente vs Agente debe jugar siempre con la funcion que Maximiza y no Max vs Min? 
 
